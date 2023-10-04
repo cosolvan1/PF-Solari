a3760e3894e439c88092cf1cf930d060f66e94fa
Ricardo Solari <133733530+cosolvan1@users.noreply.github.com>
2023-10-04 18:56:56 +0000

    avance proyecto

--- js/cards.js
+++ js/cards.js
@@ -1,28 +1,4 @@
 class Servicio {
-<<<<<<< HEAD
-    constructor(id, nombre, precio) {
-      this.id = id;
-      this.nombre = nombre;
-      this.precios = precio;
-      this.imagen = `${id}.jgp`;
-      this.altinf = `imagen del servicio ${nombre}`;
-    }
-  }
-  
-  const carrusel1 = document.getElementById("carrusel-1");
-  const carrusel2 = document.getElementById("carrusel-2");
-  
-  export const CardsServicios = (data) => {
-    carrusel1.innerHTML = "";
-    data
-      .filter((servicio) => parseInt(servicio.id) <= 5)
-      .map((servicio) => {
-        const { id, nombre, imagen, altinf } = servicio;
-  
-        let card = document.createElement("div");
-        card.className = "carrusel-track";
-        card.innerHTML = `
-=======
   constructor(id, nombre, precio) {
     this.id = id;
     this.nombre = nombre;
@@ -70,7 +46,6 @@ export const CardsServicios = (data) => {
       let card = document.createElement("div");
       card.className = "carrusel-track";
       card.innerHTML = `
->>>>>>> db23045bb2f4d414b0ebfb2437a46d9b75a07d9b
   
               <div class="carrusel">
                 <div>
@@ -83,40 +58,7 @@ export const CardsServicios = (data) => {
                 </div>
               </div>
         `;
-<<<<<<< HEAD
-  
-        carrusel1.appendChild(card);
-      });
-  
-    carrusel2.innerHTML = "";
-    data
-      .filter((servicio) => parseInt(servicio.id) > 5)
-      .map((servicio) => {
-        const { id, nombre, imagen, altinf } = servicio;
-  
-        let card = document.createElement("div");
-        card.className = "carrusel-track";
-        card.innerHTML = `
-    
-                <div class="carrusel">
-                  <div>
-                    <a href="#popupcontainer">
-                      <h4><small>${nombre}</small></h4>
-                      <picture>
-                        <img src="/images/${imagen}" alt="${altinf}" />
-                      </picture>
-                    </a>
-                  </div>
-                </div>
-          `;
-  
-        carrusel2.appendChild(card);
-      });
-  };
-  
-=======
 
       carrusel2.appendChild(card);
     });
 };
->>>>>>> db23045bb2f4d414b0ebfb2437a46d9b75a07d9b
